5d9155321c6e556a17aaeb62f79d9d568e24faec
suzzang_g <hyeonnee98@gmail.com>
2024-05-29 14:47:05 +0900

    Initial Commit

---
+++ ToDoListWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListWidget
+//
+//  Created by 이수현 on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListWidget/ToDoListWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListWidgetApp.swift
+//  ToDoListWidget
+//
+//  Created by 이수현 on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
